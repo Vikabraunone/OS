782cf8a83f5b8cd88541af50e81101b451b77559
Vikabraunone <vikabraunone@gmail.com>
2020-06-04 16:12:52 +0400

    Lab 4 ver 4

--- src/FileSystemLogic/File.java
+++ src/FileSystemLogic/File.java
@@ -7,7 +7,6 @@ public class File {
 	private String extension;
 	private int size;
 	private Link head;
-	private Link tail;
 	
 	public File(String name, String extension, int idBlock, int size)
 	{
@@ -17,7 +16,6 @@ public class File {
 		head = new Link();
 		head.idBlock = idBlock;
 		head.nextBlock = null;
-		tail = head;
 	}
 	
 	public String getName()
@@ -35,16 +33,10 @@ public class File {
 		Link item = new Link();
 		item.idBlock = idBlock;
 		item.nextBlock = null;
-		if (tail == head)
-		{
-			tail = item;
-			head.nextBlock = tail;
-		}
-		else
-		{
-			tail.nextBlock = item;
-			tail = tail.nextBlock;
-		}
+		Link itemHead = head;
+		while(itemHead.nextBlock != null)
+			itemHead = itemHead.nextBlock;
+		itemHead.nextBlock = item;
 	}
 	
 	public ArrayList<Integer> getBlocks()
@@ -53,12 +45,11 @@ public class File {
 		Link item = new Link();
 		item.idBlock = head.idBlock;
 		item.nextBlock = head.nextBlock;
-		while(item != tail)
+		while(item != null)
 		{
 			blocks.add(item.idBlock);
 			item = item.nextBlock;
 		}
-		blocks.add(tail.idBlock);
 		return blocks;
 	}
 	
